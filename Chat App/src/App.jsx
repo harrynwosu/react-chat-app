f39387d071d020dd34e68075e144977cbf508a7f
Harold Nwosu <harold.nwosu@gmail.com>
2023-10-31 03:30:37 -0500

    First round ChatFeed improvements

--- Chat App/src/App.jsx
+++ Chat App/src/App.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { ChatEngine } from "react-chat-engine";
+
+import ChatFeed from "./components/ChatFeed";
+
+
 import "./App.css";
 
 const App = () => {
@@ -10,6 +14,8 @@ const App = () => {
       projectID="a37eb189-2b03-4a35-8c8a-2769881f545d"
       userName="peterparker"
       userSecret="1234"
+      // Render custom components
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
     />
   )
 }
